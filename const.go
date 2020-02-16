e719f8c9e994c0b631c1fd2180c0864d41fac328
Douglas Richardson <dougrich@outlook.com>
2020-02-16 08:01:29 -0800

    Added constants

--- const.go
+++ const.go
@@ -3,6 +3,9 @@ package httputil
 const (
 	HeaderContentType = "Content-Type"
 	HeaderLocation = "Location"
+	HeaderForwardedProtocol = "x-forwarded-proto"
+	HeaderForwardedHost = "x-forwarded-host"
+	HeaderHost = "Host"
 	MimeTypeUrlEncodedForm = "application/x-www-form-urlencoded"
 	MimeTypeJson = "application/json"
 )
